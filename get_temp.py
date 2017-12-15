03e86a7cf5a4edf13ad1abc74de98a2947631b64
albertcode <alberto.andres.alonso@gmail.com>
2017-12-15 20:17:18 +0100

    temp average between three pws

--- get_temp.py
+++ get_temp.py
@@ -1,5 +1,6 @@
-import urllib2
+#!/usr/bin/env python
 import json
+import urllib2
 from config import Config
 from urllib import urlencode
 import random
@@ -8,21 +9,39 @@ def c_to_f(input_temp):
     # convert input_temp from Celsius to Fahrenheit
     return (input_temp * 1.8) + 32
 
+def parse_pws_conditions_json(pws, alb_key):
+    # parse json conditions from weather station
+    #with urllib2.urlopen('http://api.wunderground.com/api/'+alb_key+'/conditions/q/pws:'+pws+'.json') as json_string:  
+    f = urllib2.urlopen('http://api.wunderground.com/api/'+alb_key+'/conditions/q/pws:'+pws+'.json')
+    json_string = f.read()
+    parsed_json = json.loads(json_string)
+    f.close()
+    return parsed_json
+    
+def read_temp_c_from_pws(parsed_json):
+    # read temperature from a weather station
+    temp_c = parsed_json['current_observation']['temp_c']
+    return temp_c
 
+print("\n\nUploading data to Weather Underground\n\n")
 alb_key = 'cac064240e1597b3'
+pws_1 = 'IGULAEST2'
+pws_2 = 'IGUADALA26'
+pws_3 = 'IGUADALA29'
+json_pws_1 = parse_pws_conditions_json(pws_1,alb_key)
+json_pws_2 = parse_pws_conditions_json(pws_2,alb_key)
+json_pws_3 = parse_pws_conditions_json(pws_3,alb_key)
+temp_c_pws_1 = read_temp_c_from_pws(json_pws_1)
+temp_c_pws_2 = read_temp_c_from_pws(json_pws_2)
+temp_c_pws_3 = read_temp_c_from_pws(json_pws_3)
 
-	f = urllib2.urlopen('http://api.wunderground.com/api/'+alb_key+'/conditions/q/pws:IGULAEST2.json')
-	f2 = urllib2.urlopen('http://api.wunderground.com/api/'+alb_key+'/conditions/q/pws:IGUADALA26.json')
-	f3 = urllib2.urlopen('http://api.wunderground.com/api/'+alb_key+'/conditions/q/pws:IGUADALA29.json')
+temp_c = (temp_c_pws_1 + temp_c_pws_2 + temp_c_pws_3) / 3
 
-json_string = f.read()
-parsed_json = json.loads(json_string)
-location = parsed_json['current_observation']['observation_location']['city']
-temp_c = parsed_json['current_observation']['temp_c']
-pressure_mb = parsed_json['current_observation']['pressure_mb']
-hum = parsed_json['current_observation']['relative_humidity']
+location = json_pws_1['current_observation']['observation_location']['city']
+pressure_mb = json_pws_1['current_observation']['pressure_mb']
+hum = json_pws_1['current_observation']['relative_humidity']
 print "\nCurrent temperature and humidity in %s is: %s %s" % (location, temp_c, hum)
-f.close()
+
 
 # ============================================================================
 #  Read Weather Underground Configuration Parameters
@@ -46,31 +65,28 @@ WEATHER_UPLOAD = True
 WU_URL = "http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
 # is weather upload enabled (True)?
 if WEATHER_UPLOAD:
-	# From http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
-	print("Uploading data to Weather Underground")
-	# build a weather data object
-	weather_data = {
-		"action": "updateraw",
-		"ID": wu_station_id,
-		"PASSWORD": wu_station_key,
-		"dateutc": "now",
-		"tempc": temp_c,
-		"tempf": str(c_to_f(temp_c)),
-		"humidity": hum,
-		"baromin": pressure_mb,
-	}
-	try:
-		upload_url = WU_URL + "?" + urlencode(weather_data)
-		response = urllib2.urlopen(upload_url)
-		html = response.read()
-		print("Server response:", html)
-		# do something
-		response.close()  # best practice to close the file
-	except:
-		print("Exception:", sys.exc_info()[0], SLASH_N)
+    # From http://wiki.wunderground.com/index.php/PWS_-_Upload_Protocol
+    print("Uploading data to Weather Underground")
+    # build a weather data object
+    weather_data = {
+        "action": "updateraw",
+        "ID": wu_station_id,
+        "PASSWORD": wu_station_key,
+        "dateutc": "now",
+        "tempc": temp_c,
+        "tempf": str(c_to_f(temp_c)),
+        "humidity": hum,
+        "baromin": pressure_mb,
+    }
+    try:
+        upload_url = WU_URL + "?" + urlencode(weather_data)
+        response = urllib2.urlopen(upload_url)
+        html = response.read()
+        print("Server response:", html)
+        # do something
+        response.close()  # best practice to close the file
+    except:
+        print("Exception:", sys.exc_info()[0], SLASH_N)
 else:
-	print("Skipping Weather Underground upload")
-
-
-
+    print("Skipping Weather Underground upload")
 
