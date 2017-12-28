e14abf03bf9148bf1fac5d641a74f66c21e33690
albertcode <alberto.andres.alonso@gmail.com>
2017-12-28 22:52:52 +0100

    DHT22 functionality added

--- get_temp.py
+++ get_temp.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
+dth22_sensor = False
 import json
 import urllib2
 from config import Config
 from urllib import urlencode
-import random
+from time import sleep
+if dth22_sensor:
+    import Adafruit_DHT
 
 def c_to_f(input_temp):
     # convert input_temp from Celsius to Fahrenheit
@@ -35,6 +38,9 @@ def read_rain_from_pws(parsed_json):
     precip_today_in = parsed_json['current_observation']['precip_today_in']
     return precip_1hr_in, precip_today_in
 
+# ============================================================================
+#  Read Weather conditions from WU stations or DTH22 sensor
+# ============================================================================
 alb_key = 'cac064240e1597b3'
 pws_1 = 'IGULAEST2'
 pws_2 = 'IGUADALA26'
@@ -42,23 +48,44 @@ pws_3 = 'IMARCHAM2'
 json_pws_1 = parse_pws_conditions_json(pws_1,alb_key)
 json_pws_2 = parse_pws_conditions_json(pws_2,alb_key)
 json_pws_3 = parse_pws_conditions_json(pws_3,alb_key)
-temp_c_pws_1 = read_temp_c_from_pws(json_pws_1)
-temp_c_pws_2 = read_temp_c_from_pws(json_pws_2)
-temp_c_pws_3 = read_temp_c_from_pws(json_pws_3)
-wind_dir, windspeedmph, windgustmph = read_wind_from_pws(json_pws_1)
-rainin, dailyrainin = read_rain_from_pws(json_pws_1)
- 
-
-temp_c = (temp_c_pws_1 + temp_c_pws_2 + temp_c_pws_3) / 3
 
 location = json_pws_1['current_observation']['observation_location']['city']
 pressure_mb = json_pws_1['current_observation']['pressure_mb']
-hum = json_pws_1['current_observation']['relative_humidity']
-print "\nCurrent temperature and humidity in %s is: %s %s" % (location, temp_c, hum)
+wind_dir, windspeedmph, windgustmph = read_wind_from_pws(json_pws_1)
+rainin, dailyrainin = read_rain_from_pws(json_pws_1)
+hum = None
+temp_c = None
+n_retries = 0
 
-#dewpoint = hum^(1/8) * (112 + 0.9 * temp_c) + (0.1 * temp_c - 112)
+if dth22_sensor:
+    # DTH22 sensor is enabled
+
+    # Adafruit_DHT.DHT22
+    sensor = Adafruit_DHT.DHT22
+    
+    # Raspberry Pi with DHT sensor connected to GPIO23 (Pin 16).
+    pin = 23
+    
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    while (hum == None and temp_c == None) or n_retries == 15:
+        hum, temp_c = Adafruit_DHT.read_retry(sensor, pin)
+        n_retries +=1
+        sleep(1)
+    
+else:
+    # DTH22 sensor is disabled
+
+    hum = json_pws_1['current_observation']['relative_humidity']
+    temp_c_pws_1 = read_temp_c_from_pws(json_pws_1)
+    temp_c_pws_2 = read_temp_c_from_pws(json_pws_2)
+    temp_c_pws_3 = read_temp_c_from_pws(json_pws_3)
+    temp_c = (temp_c_pws_1 + temp_c_pws_2 + temp_c_pws_3) / 3
+
+# Calculate dewpoint as hum^(1/8) * (112 + 0.9 * temp_c) + (0.1 * temp_c - 112)
 hum_float = float(float(hum.strip('%'))/100)
 dewpoint = float((hum_float**(1./8) * (112 + 0.9 * temp_c)) + (0.1 * temp_c) - 112)
+print "\nCurrent temperature and humidity in %s is: %s %s" % (location, temp_c, hum)
 
 # ============================================================================
 #  Read Weather Underground Configuration Parameters
@@ -97,7 +124,7 @@ if WEATHER_UPLOAD:
         "dateutc": "now",
         "tempf": str(c_to_f(temp_c)),
         "humidity": hum,
-        "dewptf": dewpoint,
+        "dewptf": str(c_to_f(dewpoint)),
         "baromin": pressure_mb,
     }
     try:
